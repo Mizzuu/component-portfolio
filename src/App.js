47e267e819c27ee9b6d614c43adb930670861fca
mizzuu <mizuho.tohma72@gmail.com>
2023-02-02 18:21:00 -0800

    import Skill component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.scss";
 import Header from "./header/Header";
 import Contact from "./contact/Contact";
+import Skill from "./skill/Skill";
 
 function App() {
     return (
         <div className="home">
             <Header />
+            <Skill />
             <Contact />
         </div>
     );
